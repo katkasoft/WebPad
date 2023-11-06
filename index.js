3431ddda9ce340abca7b136061b6c3e1d60f5453
katka <katkaleks@gmail.com>
2023-11-06 15:52:34 +0300

    Removed useless db

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const app = express()
-const sqlite = require("sqlite3").verbose()
-const db = new sqlite.Database('database.db')
+// const sqlite = require("sqlite3").verbose()
+// const db = new sqlite.Database('database.db')
 const PORT = 80
 const HOST = 'localhost'
 
-db.serialize(() => {
-    db.run("CREATE TABLE IF NOT EXISTS USER (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, email TEXT)")
-})
-db.close()
+// db.serialize(() => {
+//     db.run("CREATE TABLE IF NOT EXISTS USER (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, email TEXT)")
+// })
+// db.close()
 
 app.use("/static", express.static(__dirname + "/static"));
 app.set('view engine', 'ejs');
